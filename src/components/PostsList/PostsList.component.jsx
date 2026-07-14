5cce2a12f30db8987e1d17779964536635557af4
AdminAkai <akatoirotech@gmail.com>
2026-07-14 19:57:07 -0400

    Adjusted PostsList styling

--- src/components/PostsList/PostsList.component.jsx
+++ src/components/PostsList/PostsList.component.jsx
@@ -1,7 +1,9 @@
+import styles from './PostsList.module.css';
+
 import Post from 'src/components/Post';
 
 const PostsList = () => (
-  <ul>
+  <ul className={styles.posts}>
     <Post author='test' body='test' />
   </ul>
 );
